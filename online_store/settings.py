4b2bd96686662b42b6bb97ebf5f4e71fdc2430e9
Shubhang <shubhang@Shubhangs-MacBook-Air.local>
2020-03-30 13:32:18 +0530

    fixe

--- online_store/settings.py
+++ online_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -27,7 +28,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
-
+django_heroku.settings(locals())
 # Application definition
 
 INSTALLED_APPS = [
